5536739299db83d314d5a70d12e9687c32837578
Matthew Seal <mseal007@gmail.com>
2018-06-17 00:28:05 -0700

    Added initial Sharknado implementation.

---
+++ Sharknado/IncidentWorker_Tornado.cs
@@ -0,0 +1,80 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace Sharknado
+{
+  /*
+   * Copied from decompilation from B18 -- too many items were protected to overwrite...
+   */
+  public class IncidentWorker_TornadoCopy : IncidentWorker
+  {
+    protected const int MinDistanceFromMapEdge = 30;
+
+    protected const float MinWind = 1f;
+
+    protected override bool CanFireNowSub(IIncidentTarget target)
+    {
+      Map map = (Map)target;
+      return map.weatherManager.CurWindSpeedFactor >= 1f;
+    }
+
+    protected virtual TornadoCopy Spawn(IntVec3 loc, Map map)
+    {
+      return (TornadoCopy)GenSpawn.Spawn(ThingDef.Named("Tornado"), loc, map);
+    }
+
+    protected override bool TryExecuteWorker(IncidentParms parms)
+    {
+      Map map = (Map)parms.target;
+      CellRect cellRect = CellRect.WholeMap(map).ContractedBy(30);
+      if (cellRect.IsEmpty)
+      {
+        cellRect = CellRect.WholeMap(map);
+      }
+      IntVec3 loc;
+      if (!CellFinder.TryFindRandomCellInsideWith(cellRect, (IntVec3 x) => this.CanSpawnTornadoAt(x, map), out loc))
+      {
+        return false;
+      }
+      base.SendStandardLetter(Spawn(loc, map), new string[0]);
+      return true;
+    }
+
+    protected bool CanSpawnTornadoAt(IntVec3 c, Map map)
+    {
+      if (c.Fogged(map))
+      {
+        return false;
+      }
+      int num = GenRadial.NumCellsInRadius(7f);
+      for (int i = 0; i < num; i++)
+      {
+        IntVec3 c2 = c + GenRadial.RadialPattern[i];
+        if (c2.InBounds(map))
+        {
+          if (this.AnyPawnOfPlayerFactionAt(c2, map))
+          {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    protected bool AnyPawnOfPlayerFactionAt(IntVec3 c, Map map)
+    {
+      List<Thing> thingList = c.GetThingList(map);
+      for (int i = 0; i < thingList.Count; i++)
+      {
+        Pawn pawn = thingList[i] as Pawn;
+        if (pawn != null && pawn.Faction == Faction.OfPlayer)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
---
+++ Sharknado/Sharknado.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Sharknado
+{
+  [StaticConstructorOnStartup]
+  public class Sharknado : TornadoCopy
+  {
+    protected static readonly Material SharknadoMaterial = MaterialPool.MatFrom("Things/Ethereal/Sharknado", ShaderDatabase.Transparent, MapMaterialRenderQueues.Tornado);
+
+    protected override Material GetMaterial()
+    {
+      return SharknadoMaterial;
+    }
+
+    public override void Tick()
+    {
+      if (base.Spawned && this.IsHashIntervalTick(500))
+      {
+        IntVec3 nearbyLoc = new Vector3(this.realPosition.x + Rand.Range(-10, 10), 0f, this.realPosition.y + Rand.Range(-10, 10)).ToIntVec3();
+        Pawn shark = PawnGenerator.GeneratePawn(PawnKindDef.Named("LandShark"), null);
+        Pawn spawned = (Pawn)GenSpawn.Spawn(shark, nearbyLoc, base.Map, Rot4.Random);
+        spawned.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Manhunter, null, false, false, null);
+        Log.Message("Waiting for: " + this.ticksLeftToDisappear);
+      }
+      base.Tick();
+    }
+  }
+
+  public class IncidentWorker_Sharknado : IncidentWorker_TornadoCopy
+  {
+    protected override TornadoCopy Spawn(IntVec3 loc, Map map)
+    {
+      return (TornadoCopy)GenSpawn.Spawn(ThingDef.Named("Sharknado"), loc, map);
+    }
+  }
+}
